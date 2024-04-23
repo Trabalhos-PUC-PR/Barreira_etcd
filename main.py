5aff5308d29485666910d2cfaea946f8e20354ec
Andre Luiz Kovalski <andre.kovalski@ene02-ep-pucpr.grupomarista.local>
2024-04-23 19:50:19 -0300

    barreira concluida

--- main.py
+++ main.py
@@ -3,34 +3,36 @@ import etcd3
 import time
 from sys import argv
 
-processo = (int) ( argv[1])
-
+processo = (int) (argv[1])
 client = etcd3.client()
 
+chave = 'latest'
 locks = []
 lock = ""
 for i in range(processo):
- locks.append(client.lock((str)(i)))
+    locks.append(client.lock((str)(i)))
 
 for i in locks:
- if i.acquire(timeout = 0):
-     lock = i
-     break
+    if i.acquire(timeout = 0.1):
+        lock = i
+        client.put(chave, (str)(locks.index(i)+1))
+        break
 
-print(locks[0].is_acquired())
+print(f'processo #{((int)(client.get(chave)[0]))}')
 for i in range(10):
- print(i+1)
- time.sleep(1)
+    print(i+1)
+    time.sleep(1)
 
+print("Esperando outros processos começarem para chegar na barreira")
 
-
+while(((int)(client.get(chave)[0])) != processo):
+    pass
+    
 print("Chegou na barreira")
-cout = 0
-
 lock.release()
 
 for i in locks:
- i.acquire()
- i.release()
+    i.acquire()
+    i.release()
 
 print("Saiu da Barreira")
